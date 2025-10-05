5a1311f32e92457f6b066e7014f748c4460f98ee
Harshil0408 <kanjariyaharshit@gmail.com>
2025-10-05 19:19:27 +0530

    feat(auth): implement user authentication with social login and session management
    - Added user, session, account, and verification models to Prisma schema.
    - Integrated better-auth for authentication handling.
    - Created user button component for displaying user info and logout functionality.
    - Developed sign-in page with options for GitHub and Google login.
    - Implemented authentication layout to redirect authenticated users.
    - Added environment variable handling for OAuth credentials.
    - Updated package.json with new dependencies for authentication.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { currentUser } from "@/modules/authentication/actions";
+import UserButton from "@/modules/authentication/components/user-button";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+
   return (
-    <Button>Click</Button>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <UserButton user={user}></UserButton>
+    </div>
   );
 }
